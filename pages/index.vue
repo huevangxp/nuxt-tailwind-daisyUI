3e597a7f28f4d335a281ea6b660996ab5b4b0ca8
huevangxp <huevang933@gamil.com>
2023-08-25 12:01:41 +0700

    new

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div class="bg-white">
+    <h1 class="text-red-500 text-center py-10 font-bold text-[20px] uppercase">
+      huevang
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
